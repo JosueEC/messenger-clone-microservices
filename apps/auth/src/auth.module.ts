30d9819353297d650db00f8dd499c180a9760578
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-12-11 16:33:55 -0600

    feat(auth.module): 🔨 Change the descriptive comments format and add the function forFeature for the TypeOrmModule

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserEntity } from './entity/user.entity';
 
 @Module({
   imports: [
@@ -15,12 +16,29 @@ import { AuthService } from './auth.service';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         url: configService.get('POSTGRES_URI'),
-        // autoLoadEntities: true,
-        // syncronize: true // No seberia ser usado en produccion
-        // podria perderse informacion
+        /**
+         * autoLoadEntities busca de manera automatica las clases
+         * marcadas con la anotacion @Entity, de esta forma no es
+         * necesario añadirlas de forma manual con la funcion
+         * .forFeature() de TypeOrmModule
+         */
+        autoLoadEntities: true,
+        /**
+         * Synchronize sincroniza los cambios en las entidades
+         * definidas con codigo y las creadas en la base de datos,
+         * esto significa que cuando ocurra un cambio en la entidad,
+         * esta se eliminar para crear la nueva.
+         *
+         * Dado lo anterior, esta opcion solo debe usarse en el
+         * ambiente de desarrollo, en produccion se usan migraciones
+         * para modificar entidades, ya que con estas no tenemos
+         * perdidas de informacion.
+         */
+        synchronize: true,
       }),
       inject: [ConfigService],
     }),
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
