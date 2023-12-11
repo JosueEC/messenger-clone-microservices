a2dc8968f085a9943165773c3eb9b5c80d83b6ca
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-12-11 16:31:23 -0600

    feat(auth.controller): 🔨 Add the get-users, post-user, update-user and delete-user message patterns

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { MessagePattern, Ctx, RmqContext } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
+import { UserEntity } from './entity/user.entity';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
-  }
 
   /**
    * En el caso de los microservicios consumidores, usamos el
@@ -27,12 +24,41 @@ export class AuthController {
    * De este context podemos obtener el channel y el message de la
    * request
    */
-  @MessagePattern({ cmd: 'get-user' })
-  public async getUser(@Ctx() context: RmqContext) {
+  @MessagePattern({ cmd: 'get-users' })
+  public async getUsers(
+    @Ctx() context: RmqContext,
+  ): Promise<Array<UserEntity>> {
     const channel = context.getChannelRef();
     const message = context.getMessage();
     channel.ack(message);
 
-    return { user: 'USER' };
+    return this.authService.findUsers();
+  }
+
+  @MessagePattern({ cmd: 'post-user' })
+  public async postUser(@Ctx() context: RmqContext): Promise<UserEntity> {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+
+    return this.authService.saveUser();
+  }
+
+  @MessagePattern({ cmd: 'update-user' })
+  public async updateUser(@Ctx() context: RmqContext): Promise<UpdateResult> {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+
+    return this.authService.updateUser();
+  }
+
+  @MessagePattern({ cmd: 'delete-user' })
+  public async deleteUser(@Ctx() context: RmqContext): Promise<DeleteResult> {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+
+    return this.authService.deleteUser();
   }
 }
