2428385b3aca21140ff5c24391f2ceb1931e55f6
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-12-11 16:30:30 -0600

    feat(auth.service): 🔨 Add the basic CRUD functions for a user entity

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  public async findUsers(): Promise<Array<UserEntity>> {
+    return this.userRepository.find();
+  }
+
+  public async saveUser(): Promise<UserEntity> {
+    return this.userRepository.save({ name: 'Josue' });
+  }
+
+  public async updateUser(): Promise<UpdateResult> {
+    return this.userRepository.update({ id: 1 }, { name: 'Josue Cruz' });
+  }
+
+  public async deleteUser(): Promise<DeleteResult> {
+    return this.userRepository.delete({ id: 1 });
   }
 }
