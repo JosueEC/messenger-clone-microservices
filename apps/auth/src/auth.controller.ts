d53020f8fa508b316c2f6aca53a65352974cc5c8
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-04 18:28:45 -0600

    refactor(auth.controller): ♻️ Refactor the acknowledge message form rabbitmq and add the 'register' message pattern

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,22 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Ctx, RmqContext } from '@nestjs/microservices';
+import {
+  MessagePattern,
+  Ctx,
+  RmqContext,
+  Payload,
+} from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { UserEntity } from './entity/user.entity';
 import { DeleteResult, UpdateResult } from 'typeorm';
+import { SharedService } from '@app/shared';
+import { NewUserDto } from './dto/new-user.dto';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly sharedService: SharedService,
+  ) {}
 
   /**
    * En el caso de los microservicios consumidores, usamos el
@@ -28,37 +38,47 @@ export class AuthController {
   public async getUsers(
     @Ctx() context: RmqContext,
   ): Promise<Array<UserEntity>> {
-    const channel = context.getChannelRef();
-    const message = context.getMessage();
-    channel.ack(message);
-
+    /**
+     * const channel = context.getChannelRef();
+     * const message = context.getMessage();
+     * channel.ack(message);
+     *
+     * Lo anterior, es la forma en la que notificamos a RabbitMQ de
+     * la recpecion de un nuevo mensaje.
+     *
+     * Dado que creamos una funcion que hace este proceso en el modulo
+     * lib/ especificacmente en su archivo shared.service, entonces
+     * solo basta inyectar ese servicio y hacer uso de la funcion,
+     * quedando de la siguiente forma:
+     */
+    this.sharedService.acknowledgeMessage(context);
     return this.authService.findUsers();
-  }
-
-  @MessagePattern({ cmd: 'post-user' })
-  public async postUser(@Ctx() context: RmqContext): Promise<UserEntity> {
-    const channel = context.getChannelRef();
-    const message = context.getMessage();
-    channel.ack(message);
-
-    return this.authService.saveUser();
   }
 
   @MessagePattern({ cmd: 'update-user' })
   public async updateUser(@Ctx() context: RmqContext): Promise<UpdateResult> {
-    const channel = context.getChannelRef();
-    const message = context.getMessage();
-    channel.ack(message);
-
+    this.sharedService.acknowledgeMessage(context);
     return this.authService.updateUser();
   }
 
   @MessagePattern({ cmd: 'delete-user' })
   public async deleteUser(@Ctx() context: RmqContext): Promise<DeleteResult> {
-    const channel = context.getChannelRef();
-    const message = context.getMessage();
-    channel.ack(message);
-
+    this.sharedService.acknowledgeMessage(context);
     return this.authService.deleteUser();
+  }
+
+  /**
+   * @Payload es eldecorador que nos permite recuperar informacion en
+   * las peticiones a traves de microservicios, podemos usar un objeto
+   * DTO para englobar esta carga util y asi poder recuperar la
+   * informacion que fue enviada a este endpoint de este microservicio
+   */
+  @MessagePattern({ cmd: 'register' })
+  public async registerUser(
+    @Ctx() context: RmqContext,
+    @Payload() payload: NewUserDto,
+  ): Promise<UserEntity> {
+    this.sharedService.acknowledgeMessage(context);
+    return this.authService.registerUser(payload);
   }
 }
