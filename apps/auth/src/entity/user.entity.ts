5460ebe5c5350fbac9ef1f07ecb9506a6fc2bea4
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-04 18:33:17 -0600

    refactor(auth/user.entity): ♻️ Add the 'firsName', 'lastName', 'email', and 'password' properties

--- apps/auth/src/entity/user.entity.ts
+++ apps/auth/src/entity/user.entity.ts
@@ -9,5 +9,29 @@ export class UserEntity {
     name: 'name',
     nullable: false,
   })
-  name: string;
+  firstName: string;
+
+  @Column({
+    name: 'last_name',
+  })
+  lastName: string;
+
+  @Column({
+    name: 'email',
+    unique: true,
+  })
+  email: string;
+
+  /**
+   * * select: false
+   * Nos permite indicar que la columna marcada con esta propiedad
+   * no sera devuelta al cliente en ningun tipo de consulta, de esta
+   * forma evitamos que el password sea devuelto al cliente, lo cual
+   * seria un problema de seguridad.
+   */
+  @Column({
+    name: 'password',
+    select: false,
+  })
+  password: string;
 }
