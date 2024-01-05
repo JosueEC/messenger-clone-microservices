d4293e0dcd6b85b9385d13a7a5d58dab2f2bf74c
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-04 18:30:26 -0600

    feat(auth.service): 🔨 Add the 'findByEmail', 'hashPassword' and 'registerUser' functions and delete the 'saveUser' function

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { NewUserDto } from './dto/new-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,12 +16,61 @@ export class AuthService {
     return this.userRepository.find();
   }
 
-  public async saveUser(): Promise<UserEntity> {
-    return this.userRepository.save({ name: 'Josue' });
+  /**
+   * * select
+   * Nos permite sobreescribir que parametros deseamos que sean
+   * devueltos en la consulta, esto sobreescribe la propiedad
+   * select: false en la entidad UserEntity, por lo tanto, esta
+   * funcion si nos devuelve la password, ya que esta, sera necesaria.
+   */
+  public async findByEmail(email: string): Promise<UserEntity> {
+    return this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'firstName', 'lastName', 'email', 'password'],
+    });
+  }
+
+  /**
+   * El numero que se pasa como parametro en la funcion .hash es el
+   * numero de veces que se hashea la contraseña recibida como primer
+   * parametro
+   */
+  public async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  /**
+   * * Readonly<>
+   * Nos permite indicar que, para el objeto que se esta recibiendo
+   * se pueden leer/acceder a sus propiedades, pero estas no pueden
+   * ser mutadas/modificadas.
+   */
+  public async registerUser(
+    payload: Readonly<NewUserDto>,
+  ): Promise<UserEntity> {
+    const { firstName, lastName, email, password } = payload;
+
+    const existingUser = await this.findByEmail(email);
+
+    if (existingUser) {
+      throw new ConflictException('An account with that email already exist');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const savedUser = await this.userRepository.save({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    delete savedUser.password;
+    return savedUser;
   }
 
   public async updateUser(): Promise<UpdateResult> {
-    return this.userRepository.update({ id: 1 }, { name: 'Josue Cruz' });
+    return this.userRepository.update({ id: 1 }, { firstName: 'Josue Cruz' });
   }
 
   public async deleteUser(): Promise<DeleteResult> {
