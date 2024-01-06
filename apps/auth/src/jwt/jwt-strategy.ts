cf4942fe1324963d9fb412af63e5dd21feb5b8ce
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-05 20:10:03 -0600

    feat(auth/jwt): 🔨 Add the jwt-strategy with the ExtractJwt method

---
+++ apps/auth/src/jwt/jwt-strategy.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtRequest } from './jwt-request';
+
+/**
+ * En esta clase, estamos pasando al constructor de la clase
+ * PassprtStrategy un objeto con el token y algunas propiedades
+ * mas necesarias para la misma.
+ *
+ * * ExtractJwt
+ * Es una funcion de passport-jwt la cual recupera el token de la
+ * request recibida. Como puedes ver, estamos usando la interface
+ * que creamos para JWT donde inyectamos la propiedad jwt
+ */
+@Injectable()
+export class JwtStartegy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: JwtRequest) => {
+          return request?.jwt;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  public async validate(payload: any) {
+    return { ...payload };
+  }
+}
