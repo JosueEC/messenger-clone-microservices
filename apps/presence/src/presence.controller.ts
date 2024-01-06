aaabcdc2c4f91574841916ba2c2cadb066ba040e
J Evangelista Cruz <josueev.cruz@gmail.com>
2024-01-05 20:15:08 -0600

    feat(presence.controller): 🔨 Remove the temporal authGuard delcarations

--- apps/presence/src/presence.controller.ts
+++ apps/presence/src/presence.controller.ts
@@ -1,7 +1,6 @@
 import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 import { Controller } from '@nestjs/common';
 import { SharedService } from '@app/shared';
-import { AuthGuard } from '@app/shared';
 import { PresenceService } from './presence.service';
 
 @Controller()
@@ -9,15 +8,12 @@ export class PresenceController {
   constructor(
     private readonly presenceService: PresenceService,
     private readonly sharedService: SharedService,
-    private readonly authGuard: AuthGuard,
   ) {}
 
   @MessagePattern({ cmd: 'get-presence' })
   public async getPresence(@Ctx() context: RmqContext) {
     this.sharedService.acknowledgeMessage(context);
 
-    // TEMP
-    console.log(123, this.authGuard.hasJWT());
     return this.presenceService.getHello();
   }
 }
